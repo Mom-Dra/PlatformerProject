77eeccdbad1fcbbbbbaf2668eff795d75fabec78
Mom-Dra <yem9087@gmail.com>
2024-08-24 01:55:24 +0900

    독수리 공격 위치 계산

--- Assets/TheKiwiCoder/BehaviourTree/Scripts/Composites/FollowingTargetAction.cs
+++ Assets/TheKiwiCoder/BehaviourTree/Scripts/Composites/FollowingTargetAction.cs
@@ -5,6 +5,8 @@ using TheKiwiCoder;
 
 public class FollowingTargetAction : ActionNode
 {
+    public float detectDistance;
+    public float attackDistance;
     public float distance;
 
     protected override void OnStart()
@@ -19,17 +21,30 @@ public class FollowingTargetAction : ActionNode
 
     protected override State OnUpdate()
     {
-        if(blackboard.targetTransform == null)
+        float contextToTargetDistance = Vector3.Distance(context.transform.position, blackboard.targetTransform.position);
+
+        if (blackboard.targetTransform == null)
+        {
+            return State.Failure;
+        }
+        else if(contextToTargetDistance > detectDistance)
         {
             return State.Failure;
         }
 
-        if(Vector3.Distance(context.transform.position, blackboard.targetTransform.position) < distance)
-        {
+        // y좌표: AttackDistance x Sin(60도)
+        Vector3 attackPos;
+        Vector3 direction = (context.transform.position - blackboard.targetTransform.position);
+        direction.y = 0f;
+        direction = direction.normalized;
+        direction.y = Mathf.Sin(Mathf.Deg2Rad * 60f);
+        attackPos = direction.normalized * attackDistance;
 
+        // 공격 위치까지 context를 움직인다
+        // 공격 위치까지 충분히 가깝다면 Success를 반환한다
 
-            return State.Running;
-        }
+        
+
 
         return State.Success;
     }
