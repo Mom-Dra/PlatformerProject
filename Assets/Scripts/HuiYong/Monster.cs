1aefc71a0d80fc2d5d6fd069bca4392225ad3ee0
MomDra <yem9087@gmail.com>
2024-09-19 21:50:01 +0900

    (임시) 몬스터 피격

--- Assets/Scripts/HuiYong/Monster.cs
+++ Assets/Scripts/HuiYong/Monster.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TheKiwiCoder;
 using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    [SerializeField]
+    private int hp = 3;
+
+    BehaviourTreeRunner runner;
+
     private void Awake()
     {
-        
+        runner = GetComponent<BehaviourTreeRunner>();
     }
 
-    private void Update()
+    public void TakeDamage(int val)
     {
-        
+        hp -= val;
+
+        if(hp <= 0)
+        {
+            runner.enabled = false;
+        }
+        else
+        {
+            // 피격 에니메이션!
+
+
+        }
     }
 }
